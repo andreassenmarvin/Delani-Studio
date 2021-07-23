9251db4e45cda3597c572721db939fbaeaa8a677
Marvin Maina <machariamarvin625@gmail.com>
2021-07-23 20:25:51 +0300

    Add validation to form using jquery

--- js/script.js
+++ js/script.js
@@ -27,5 +27,53 @@ $(function(){
         $(".p-mgt-p").hide();
         $(".mgt-img").toggle();
     })
+
+    $("form").submit(function(event){
+
+        var inputText = document.getElementById("input-text").value;
+
+        if ($(".input-text").val() === "" || $(".input-text").val().length < 3 ){
+            $(".input-text").addClass("error");
+            $(".name-error").show();
+            $(".input-email").removeClass("error");
+            $(".email-error").hide();
+            $("#message").removeClass("error");
+            $(".message-error").hide();
+            return false;
+        }
+
+        else if ($(".input-email").val() === ""){
+            $(".input-email").addClass("error");
+            $(".email-error").show();
+            $(".input-text").removeClass("error");
+            $(".name-error").hide();
+            $("#message").removeClass("error");
+            $(".message-error").hide();
+            return false;
+        }
+
+        else if ($("#message").val() === ""){
+            $("#message").addClass("error");
+            $(".message-error").show();
+            $(".input-email").removeClass("error");
+            $(".email-error").hide();
+            $(".input-text").removeClass("error");
+            $(".name-error").hide();
+        }
+
+        else {
+            $(".input-text").removeClass("error");
+            $(".name-error").hide();
+            $(".input-email").removeClass("error");
+            $(".email-error").hide();
+            $("#message").removeClass("error");
+            $(".message-error").hide();
+            alert(`Hello ${inputText}!Your message has been sent`)
+        }
+
+
+        event.preventDefault();
+    })
     
 })
+
